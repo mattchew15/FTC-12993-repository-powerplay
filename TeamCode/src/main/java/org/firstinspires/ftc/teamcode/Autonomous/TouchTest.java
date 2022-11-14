11b28e33d35523562fafcf2e1a65068edf38fbac
Mika <mik-mey@outlook.de>
2022-11-14 22:42:41 +1000

    touch sensors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TouchTest.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import android.text.method.Touch;
+
+import com.qualcomm.hardware.rev.RevTouchSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp(name = "Sensor: RevTouchSensor", group = "Sensor")
+
+public class TouchTest extends LinearOpMode {
+
+    private DigitalChannel sensorTouchClaw;
+
+    @Override
+    public void runOpMode(){
+
+        sensorTouchClaw = hardwareMap.get(DigitalChannel.class, "sensor_touchClaw");
+
+        sensorTouchClaw.setMode(DigitalChannel.Mode.INPUT);
+
+        telemetry.addData(">>", "Press start to continue");
+        telemetry.update();
+
+        waitForStart();
+        while(opModeIsActive()) {
+
+            if(sensorTouchClaw.getState() == true){
+                telemetry.addData("Digital touch", "Is pressed");
+            }
+            else {
+                telemetry.addData("Touch shit", "is not pressed");
+            }
+
+            telemetry.update();
+
+        }
+    }
+}
+
+
