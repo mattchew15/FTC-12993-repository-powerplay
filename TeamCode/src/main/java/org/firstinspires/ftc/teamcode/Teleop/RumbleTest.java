7f14b9262f765d97210f5eb137f492645c3f9270
Mika <mik-mey@outlook.de>
2022-11-18 16:15:39 +1000

    rumble test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/RumbleTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.Teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Rumble: Controllers", group = "Controller")
+
+public class RumbleTest extends LinearOpMode{
+
+    Gamepad.RumbleEffect customRumbleEffect;
+
+    @Override
+    public void runOpMode(){
+
+        customRumbleEffect = new Gamepad.RumbleEffect.Builder()
+                .addStep(0.0, 1.0, 500)  //  Rumble right motor 100% for 500 mSec
+                .addStep(0.0, 0.0, 300)  //  Pause for 300 mSec
+                .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
+                .addStep(0.0, 0.0, 250)  //  Pause for 250 mSec
+                .addStep(1.0, 0.0, 250)  //  Rumble left motor 100% for 250 mSec
+                .build();
+
+        waitForStart();
+        while(opModeIsActive()) {
+
+            if(gamepad1.a){
+                gamepad1.runRumbleEffect(customRumbleEffect);
+            }
+            else if(gamepad1.b){
+                gamepad1.stopRumble();
+            }
+
+        }
+    }
+}
