3b4032a03680345bf0ec3728fe6977ac4175163f
Mika <mik-mey@outlook.de>
2023-01-19 17:50:03 +1000

    Configs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configs/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configs/DriveBase.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Configs;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @Config
@@ -20,6 +21,19 @@ public class DriveBase {
         DFLM = hwMap.get(DcMotor.class, "FL");
         DFRM = hwMap.get(DcMotor.class, "FR");
 
+    }
+
+    public void motorsSetup(){
+        // zero brake behavior means when motors aren't powered, they will auto brake
+        DFRM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DBRM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DBLM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DFLM.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //reverse correct motors
+        DFLM.setDirection(DcMotorSimple.Direction.REVERSE); //DcMotorSimple class?
+        DBLM.setDirection(DcMotorSimple.Direction.REVERSE);
+        DBRM.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
 }
