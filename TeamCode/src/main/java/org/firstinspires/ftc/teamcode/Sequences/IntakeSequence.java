e03239d5be9bc498b94905a12849207adb745b0b
Mika <mik-mey@outlook.de>
2023-02-10 15:23:22 +1000

    configs and gradle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sequences/IntakeSequence.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Sequences;
+
+public class IntakeSequence {
+
+    boolean intakeReady;
+    boolean clawReady;
+    boolean liftReady;
+    boolean slidesReady;
+    boolean intakeSpin;
+    boolean intakeReverseSpin;
+
+    enum NormalIntakeEnum{
+        INTAKE_IN,
+        INTAKE_OUT,
+        REST,
+        GRAB,
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sequences/OutakeSequence.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.teamcode.Sequences;
+
+public class OutakeSequence {
+
+}
